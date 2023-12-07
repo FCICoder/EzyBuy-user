c99f39b0214a14db7a7f614448112670f6e51a3a
FCICoder <mahmoud.aadel1998@gmail.com>
2023-12-07 07:19:42 +0200

    gg''

--- src/Components/Password/Password.jsx
+++ src/Components/Password/Password.jsx
@@ -62,7 +62,7 @@ export default function Password() {
   let validateSchema = Yup.object({
     password: Yup.string()
       .matches(
-        /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
+        /^(?=.*[A-Za-z])[A-Za-z\d]{6,}$/,
         "password is invalid start with upper case and min is 8 char"
       )
       .required("password is required"),
